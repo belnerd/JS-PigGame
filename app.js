fbce155f88292cf7c5b10b5a4e36c6363cc1b282
Tapio Ekman <42242320+belnerd@users.noreply.github.com>
2020-09-21 21:58:24 +0300

    Add files via upload
    Version 2 with the extended rules:
    - Ability to change the goal of the game
    - There are now 2 dice in play
    - If one of the dice is a 1 the player keeps the total score but the player changes
    - If a player rolls two sixes in a row the player looses his total score and the player changes.

--- app.js
+++ app.js
@@ -15,36 +15,47 @@ score of 100.
 3. Add another dice to the game so that there are two dices now. The player looses his current score if one of them is a 1.
 */
 
-var scores, roundScore, activePlayer, previousRoll, dice;
+var scores, roundScore, activePlayer, previousRoll, dice, maxScore;
 newGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
     // 1. random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    dice[0] = Math.floor(Math.random() * 6) + 1;
+    dice[1] = Math.floor(Math.random() * 6) + 1;
 
     // 2. display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.src = 'dice-' + dice[0] + '.png';
+    diceDOM = document.querySelector('.dice2');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice[1] + '.png';
     
     // 3. update the round score if the rolled number was NOT 1
-    if (dice !== 1) {
-        // Check for two sixes in a row
-        if (dice == 6 && previousRoll[activePlayer] == 6) {
+    if (dice[0] !== 1 && dice[1] !== 1) {
+        // Check for two sixes in a row which causes the current player to lose all points
+        if ((dice[0] == 6 || dice[1] == 6) && (previousRoll[activePlayer] == 6)) {
             scores[activePlayer] = 0;
             document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+            document.getElementById('message-' + activePlayer).textContent = 'Rolled two sixes';
             nextPlayer();
+        } else if (dice[0] == 6 || dice[1] == 6) {
+            previousRoll[activePlayer] = 6;
+            roundScore = roundScore + dice[0] + dice[1];
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
         // add score
-        roundScore += dice;
-        previousRoll[activePlayer] = dice;
-        console.log(previousRoll[activePlayer]);
+        previousRoll[activePlayer] = 0;
+        roundScore = roundScore + dice[0] + dice[1];
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }} else {
+        }
+    } else {
         // change the player
+        previousRoll[activePlayer] = 0;
+        document.getElementById('message-' + activePlayer).textContent = 'Rolled a one';
         nextPlayer();
-    }   
+    }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
@@ -54,7 +65,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= maxScore) {
         document.querySelector('#score-' + activePlayer).textContent = 'WINS!';
         document.querySelector('.btn-roll').style.display = 'none';
         document.querySelector('.btn-hold').style.display = 'none';
@@ -66,9 +77,14 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 document.querySelector('.btn-new').addEventListener('click', newGame);
 
+function setMaxScore() {
+    x = document.getElementById('goal');
+    maxScore = x.value;
+}
+
 function nextPlayer() {
-    previousRoll[activePlayer] = dice;
-    console.log(previousRoll[activePlayer]);
+    // previousRoll[activePlayer] = dice;
+    // console.log(previousRoll[activePlayer]);
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
         
@@ -79,34 +95,35 @@ function nextPlayer() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice2').style.display = 'none';
+        document.getElementById('message-' + activePlayer).textContent = ' ';
 }
 
 function newGame() {
      // Reset scores
-     scores = [0,0];
-     roundScore = 0;
-     activePlayer = 0;
-     previousRoll = [0,0];
-     // Show controls & hide the dice
-     document.querySelector('.btn-roll').style.display = 'block';
-     document.querySelector('.btn-hold').style.display = 'block';
-     document.querySelector('.dice').style.display = 'none';
-     // Update UI
-     document.getElementById('score-0').textContent = '0';
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    previousRoll = [0,0];
+    dice = [0,0];
+    setMaxScore();
+    // Show controls & hide the dice
+    document.querySelector('.btn-roll').style.display = 'block';
+    document.querySelector('.btn-hold').style.display = 'block';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
+    // Update UI
+    document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+    document.getElementById('message-0').textContent = ' ';
+    document.getElementById('message-1').textContent = ' ';
+    
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
 }
 
-
-
-
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-// var x = document.querySelector('#score-0').textContent;
 
 
 
